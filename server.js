34b06d811b7d650f85166502f5ca8d718b1d144b
Alan J Fitzpatrick <alajfit@gmail.com>
2015-05-04 09:10:47 +0100

    Notes added to Node Server of what it is doing

--- server.js
+++ server.js
@@ -16,24 +16,34 @@ var mimeTypes = {
 
 // Create Server
 http.createServer(function(req, res){
+    // We must gather the path name from the URL (we pass the req url)
     var uri = url.parse(req.url).pathname;
+    // process.cwd gets the current working directory, here we join with the path
     var fileName = path.join(process.cwd(),unescape(uri));
     console.log('Loading ' + uri);
     var stats;
 
+    // We now attempt to gather the file or show a 404 and log the error message
     try {
         stats = fs.lstatSync(fileName);
     } catch(e) {
-        res.writeHead(404, {'Content-type' : 'text/plain'});
-        res.write('404 Not Found\n');
+        res.writeHead(302, {
+            'Location' : '404.html'
+        });
+        // res.writeHead(404, { 'Content-type' : 'text/plain' });
+        //res.write('404 Not Found\n');
         res.end();
+        console.log(e.message);
         return;
     }
 
     // Check if file/ directory
     if(stats.isFile()) {
+        // We must get the file extension passed the split the URL by the dot and rever
+        // so the file extension is the first element
         var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
         res.writeHead(200, {'Content-type' : mimeType});
+        console.log('200 - ' + fileName +' found and displayed');
 
         var fileStream = fs.createReadStream(fileName);
         fileStream.pipe(res);
@@ -47,4 +57,4 @@ http.createServer(function(req, res){
         res.write('500 Internal Error\n');
         res.end();
     }
-}).listen(3000);
+}).listen(3000); console.log("Loading and Listening on http://127.0.0.1:3000/index.html");
